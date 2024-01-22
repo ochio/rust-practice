a6bf82cf065358c7d9ca9068585d574253f6c4aa
ochi <ocos54ce@gmail.com>
2024-01-22 09:58:44 +0900

    hello world

--- helloworld/src/hello.rs
+++ helloworld/src/hello.rs
@@ -8,8 +8,8 @@
 
 // This is the main function.
 fn main() {
-  // Statements here are executed when the compiled binary is called.
+    // Statements here are executed when the compiled binary is called.
 
-  // Print text to the console.
-  println!("Hello World!");
+    // Print text to the console.
+    println!("Hello World!");
 }
